4bf853adbd8bcf4ec023c49eff35b26162367da0
Anatolii Prylutskyi <anpryl@gmail.com>
2018-04-08 01:06:46 +0300

    Move errors im separate pkg

---
+++ imagesvc/imgerrors/imgerrors.go
@@ -0,0 +1,29 @@
+package imgerrors
+
+import (
+	"net/http"
+)
+
+type ImageServiceError struct {
+	StatusCode int    `json:"code"`
+	Message    string `json:"message"`
+}
+
+func (i *ImageServiceError) Error() string {
+	return i.Message
+}
+
+var (
+	EmptyFilename = &ImageServiceError{
+		StatusCode: http.StatusBadRequest,
+		Message:    "Empty filename",
+	}
+	FileExist = &ImageServiceError{
+		StatusCode: http.StatusBadRequest,
+		Message:    "File exist",
+	}
+	FileNotFound = &ImageServiceError{
+		StatusCode: http.StatusNotFound,
+		Message:    "File not found",
+	}
+)
