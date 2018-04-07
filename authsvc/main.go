501abec74d070e0662eaaf78a02296d1e6c8b169
Anatolii Prylutskyi <anpryl@gmail.com>
2018-04-07 09:21:54 +0300

    Renaming some variables

--- authsvc/main.go
+++ authsvc/main.go
@@ -14,25 +14,30 @@ import (
 func main() {
 	var host = flag.String("host", "127.0.0.1", "Bind to host address (default: 127.0.0.1)")
 	var port = flag.Int("port", 80, "Use port for clients (default: 80)")
-	var secret = flag.String("secret", "", "Secret used to sign and verify tokens")
+	var secret = flag.String("secret", "", "Secret used to sign auth tokens")
 	var tokenDuration = flag.Duration(
 		"token_duration",
 		5*time.Minute,
 		"Duration after token expires (default: 5m)",
 	)
+
 	flag.Parse()
+
+	if *secret == "" {
+		log.Fatalln("secret is empty, please provide some secret with -secret flag")
+	}
 
 	mux := http.NewServeMux()
 
-	signToken := signToken(*secret, *tokenDuration)
+	signToken := tokenSigner(*secret, *tokenDuration)
 	mux.HandleFunc("/token", newTokenHandler(signToken))
 
 	log.Fatalln(http.ListenAndServe(*host+":"+fmt.Sprint(*port), mux))
 }
 
-type signedTokenFunc func() (string, error)
+type signTokenFunc func() (string, error)
 
-func signToken(secret string, tokenDuration time.Duration) signedTokenFunc {
+func tokenSigner(secret string, tokenDuration time.Duration) signTokenFunc {
 	return func() (string, error) {
 		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(tokenDuration).Unix(),
@@ -45,12 +50,12 @@ type TokenResp struct {
 	Token string `json:"token"`
 }
 
-func newTokenHandler(signedToken signedTokenFunc) http.HandlerFunc {
+func newTokenHandler(signToken signTokenFunc) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			rw.WriteHeader(http.StatusMethodNotAllowed)
 		}
-		token, err := signedToken()
+		token, err := signToken()
 		if err != nil {
 			log.Println("Error on token signing: ", err)
 			rw.WriteHeader(http.StatusInternalServerError)
